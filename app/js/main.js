dc76a7e6d4ad8873b6f4740ee3f85375d4a60efc
James Rico <jamesrico089@gmail.com>
2015-02-14 13:36:37 -0600

    posted new profile to firebase functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,17 @@
 /* jshint browser: true, jquery: true */
 
 'use strict';
+/////global variables
+  var $loginform = $('#loginform');
+  var emaillogin    = $loginform.find('[type="email"]').val();
+  var pass       = $loginform.find('[type="password"]').val();
+  var url = 'https://datingappnssc8.firebaseio.com/';
 
-/////the login page will need to authenticate and display app
+  //////for some reason not allowing me to create a Constructor object
+  //with var ref = new Firebase(url); it says that Firebase is undefined
 
-$('#loginform').on('click', '#loginbutton', function (event){
-  event.preventDefault();
 
-////hides the loginform
-  $('#loginform').toggleClass('hidden');
-///displays profile of user
-  $('#profileapp').toggleClass('hidden');
-
-})
-
-////the register app will need to hide login and display form
+////the register button will need to hide login and display form
 
 $('#loginform').on('click', '#registerbutton', function (event){
   event.preventDefault();
@@ -26,4 +23,47 @@ $('#loginform').on('click', '#registerbutton', function (event){
 })
 
 
+$('#newprofileform').on('click', '#createbutton', function createProfile(event){
+///////post this info to firebase
+
+$.post(jsonurl, profiles, function(res){
+      $npphoto.attr('profiles-uuid', res.profile)
+      $tbody.attr('profiles-uuid', res.profile);
+      })
+
+  $('#createprofile')toggleClass('hidden');
+  $('#profileapp').toggleClass('hidden');
+})
+
+function createProfile (){
+    var $tbody = $('<tbody></tbody>')
+      photo = $('#infophoto').val(),
+      name  = $('#infoname').val(),
+      gender = $('#infogender').val(),
+      email = $('#infoemail').val(),
+      descr = $('#infodescription').val(),
+      $npphoto = $('<img src='+photo+'></img>'),
+      $npname = $('<td>'+name+'<td>'),
+      $npgender = $('<td>'+gender+'<td>'),
+      $npemail = $('<td>'+email+'<td>'),
+      $npdescr =$('<td>'+descr+'<td>'),
+      jsonurl = 'https://datingappnssc8.firebaseio.com/.json';
+      profiles = JSON.stringify({photourl: photo, name: name, gender: gender, email: email, description: descr});
+}
+
+function addProfileToTable (uuid, info){
+  var $profilephoto = $('<img src='+info.photourl+'></img>'),
+      $profilename  = $('<td>'+info.name+'</td>'),
+      $profilegender  = $('<td>'+info.gender+'</td>'),
+      $profileemail = $('<td>'+info.email+'</td>'),
+      $profiledescr  = $('<td>'+info.descr+'</td>');
+///////append the info to the div and table
+
+
+      $('#imagecontainer').append($profilephoto);
+      $('#profilename').append($profilename);
+      $('#gender').append($profilegender);
+      $('#profileemail').append($profileemail);
+      $('#profiledescr').append($profiledescr);
+}
 
