184bac0b5c5a3d5762fde24bc2cbcbf37b56fcdc
James Rico <jamesrico089@gmail.com>
2015-02-15 11:55:31 -0600

    added a test in the maintestjs

---
+++ test/spec/main.test.js
@@ -0,0 +1,10 @@
+/* jshint mocha: true, expr: true, strict: false, undef: false */
+
+describe('test suite', function () {
+  it('should assert true', function () {
+    true.should.be.true;
+    false.should.be.false;
+  });
+});
+
+
